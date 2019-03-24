d7b12f86e0af2479b41cf1740150b3252b9bf4aa
Richard <rlshep@gmail.com>
2019-03-23 21:55:27 -0400

    add allowed languages

--- src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++ src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
@@ -1,9 +1,14 @@
 package io.github.rlshep.bjcp2015beerstyles.constants;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BjcpConstants {
     public static final String DATABASE_NAME = "BjcpBeerStyles.db";
     public static final int DATABASE_VERSION = 10;   // Make sure LoadBjcp project picks up this change for PRAGMA header
     public static final String UNIT = "unit";
     public static final String METRIC = "metric";
     public static final String IMPERIAL = "imperial";
+    private static final String[] ALLOWED_LANGUAGES = {"en", "es"};
+    private static final List allowedLanguages = Arrays.asList(ALLOWED_LANGUAGES);
 }
