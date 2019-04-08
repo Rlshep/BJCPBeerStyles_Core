04da1b403a855c9151d2c3af62d03269d063a555
Richard <rlshep@gmail.com>
2019-04-07 20:15:04 -0400

    separating preference units

--- src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++ src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
@@ -6,12 +6,7 @@ import java.util.List;
 public class BjcpConstants {
     public static final String DATABASE_NAME = "BjcpBeerStyles.db";
     public static final int DATABASE_VERSION = 11;   // Make sure LoadBjcp project picks up this change for PRAGMA header
-    public static final String UNIT = "unit";
-    public static final String METRIC = "metric";
-    public static final String IMPERIAL = "imperial";
+    public static final int MAX_SEARCH_CHARS = 3;
     public static final String DEFAULT_LANGUAGE = "en";
     public static final String DEFAULT_COUNTRY = "US";
-    public static final String[] ALLOWED_LANGUAGES = {"en", "es"};
-    public static final List allowedLanguages = Arrays.asList(ALLOWED_LANGUAGES);
-    public static final int MAX_SEARCH_CHARS = 3;
 }
