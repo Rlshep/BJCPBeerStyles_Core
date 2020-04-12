535cf09a049d3d1de24d3d70149a3e3e68ce75f5
Richard <rlshep@gmail.com>
2020-04-11 20:19:31 -0400

    Update to Ukraine version.

--- src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++ src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public class BjcpConstants {
     public static final String DATABASE_NAME = "BjcpBeerStyles.db";
-    public static final int DATABASE_VERSION = 16;   // Make sure LoadBjcp project picks up this change for PRAGMA header
+    public static final int DATABASE_VERSION = 17;   // Make sure LoadBjcp project picks up this change for PRAGMA header
     public static final int MAX_SEARCH_CHARS = 3;
     public static final String DEFAULT_LANGUAGE = "en";
     public static final String DEFAULT_COUNTRY = "US";
     public static final String SPANISH_HYBRID = "es_en";
     public static final String SPANISH = "es";
-    public static final String[] ALLOWED_LANGUAGES = {"en", "es", "es_en"};
+    public static final String UKRANIAN = "uk";
+    public static final String[] ALLOWED_LANGUAGES = {"en", "es", "es_en", "uk"};
     public static final List allowedLanguages = Arrays.asList(ALLOWED_LANGUAGES);
 }
