401309c2916b1b63c2dd843b8c1641e7e6d1203b
Richard <rlshep@gmail.com>
2020-04-26 11:21:26 -0400

    ukrainian update

--- src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++ src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class BjcpConstants {
     public static final String DATABASE_NAME = "BjcpBeerStyles.db";
-    public static final int DATABASE_VERSION = 18;   // Make sure LoadBjcp project picks up this change for PRAGMA header
+    public static final int DATABASE_VERSION = 19;   // Make sure LoadBjcp project picks up this change for PRAGMA header
     public static final int MAX_SEARCH_CHARS = 3;
     public static final String DEFAULT_LANGUAGE = "en";
     public static final String DEFAULT_COUNTRY = "US";
