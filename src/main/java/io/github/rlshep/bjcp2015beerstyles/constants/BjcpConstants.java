b5e57a875cd710aef2f3417c7c402ebc6f437be9
Richard Sheppard <BeerStyles2015@gmail.com>
2022-01-08 16:26:35 -0500

    db version change

--- src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++ src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class BjcpConstants {
     public static final String DATABASE_NAME = "BjcpBeerStyles.db";
-    public static final int DATABASE_VERSION = 21;   // Make sure LoadBjcp project picks up this change for PRAGMA header
+    public static final int DATABASE_VERSION = 22;   // Make sure LoadBjcp project picks up this change for PRAGMA header
     public static final int MAX_SEARCH_CHARS = 3;
     public static final String DEFAULT_LANGUAGE = "en";
     public static final String DEFAULT_COUNTRY = "US";
