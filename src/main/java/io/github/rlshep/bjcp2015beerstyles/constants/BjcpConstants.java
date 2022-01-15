310c9edbfe8483b477177064214477f5cf855b87
Richard Sheppard <BeerStyles2015@gmail.com>
2022-01-14 19:20:39 -0500

    increase db #

--- src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++ src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class BjcpConstants {
     public static final String DATABASE_NAME = "BjcpBeerStyles.db";
-    public static final int DATABASE_VERSION = 22;   // Make sure LoadBjcp project picks up this change for PRAGMA header
+    public static final int DATABASE_VERSION = 23;   // Make sure LoadBjcp project picks up this change for PRAGMA header
     public static final int MAX_SEARCH_CHARS = 3;
     public static final String DEFAULT_LANGUAGE = "en";
     public static final String DEFAULT_COUNTRY = "US";
     public static final String ENGLISH = "en";
     public static final String SPANISH = "es";
     public static final String UKRANIAN = "uk";
-    public static final String[] ALLOWED_LANGUAGES = {"en", "es", "es_en", "uk"};
+    public static final String[] ALLOWED_LANGUAGES = {"en", "es", "uk"};
     public static final List allowedLanguages = Arrays.asList(ALLOWED_LANGUAGES);
     public static final String BJCP_2015 = "BJCP_2015";
     public static final String BJCP_2021 = "BJCP_2021";
